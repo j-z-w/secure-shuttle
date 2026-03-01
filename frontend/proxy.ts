4ab7af331c588f37f91e3088e2b5be5b8b5d3a7d
Riley <RWMB4200@gmail.com>
2026-02-28 19:12:57 -0800

    Added non-admin redirect from dashboard url direct

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,12 +1,44 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
 
-export default clerkMiddleware();
+const isAdminRoute = createRouteMatcher(["/dashboard"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isAdminRoute(req)) {
+    const { sessionClaims } = await auth();
+    if (sessionClaims?.metadata?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
+
+// export const config = {
+//   matcher: [
+//     // Skip Next.js internals and all static files, unless found in search params
+//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+//     // Always run for API routes
+//     '/(api|trpc)(.*)',
+//   ],
+// };
+
+
+
+
+
+// import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+// import { NextResponse } from "next/server";
+
+// const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
+
+// export default clerkMiddleware(async (auth, req) => {
+//   if (isAdminRoute(req)) {
+//     const { sessionClaims } = await auth();
+//     if (sessionClaims?.metadata?.role !== "admin") {
+//       return NextResponse.redirect(new URL("/", req.url));
+//     }
+//   }
+// });
 
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/((?!_next|.*\\..*).*)"],
 };
