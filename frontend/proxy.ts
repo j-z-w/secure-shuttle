a4b6b02f76c7a13e7cd570c60df8c4c35db39831
Riley <RWMB4200@gmail.com>
2026-02-28 19:26:56 -0800

    Added redirect from escrow creation if not signed in

--- frontend/proxy.ts
+++ frontend/proxy.ts
@@ -1,13 +1,25 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher} from '@clerk/nextjs/server';
 import { NextResponse } from "next/server";
 
 const isAdminRoute = createRouteMatcher(["/dashboard"]);
 
+const isSignedRoute = createRouteMatcher(["/newEscrow"])
+
 export default clerkMiddleware(async (auth, req) => {
+
   if (isAdminRoute(req)) {
     const { sessionClaims } = await auth();
     if (sessionClaims?.metadata?.role !== "admin") {
       return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+  // } else if (!userID) {
+  //   return NextResponse.redirect(new URL("/signup", req.url))
+  if (isSignedRoute(req)) {
+    const { userId } = await auth();
+    console.log(userId);
+    if (!userId) {
+      return NextResponse.redirect(new URL("/signin", req.url));
     }
   }
 });
